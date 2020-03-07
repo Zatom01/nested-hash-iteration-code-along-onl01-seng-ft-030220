1d6ef6678a56ed15e4e6eb530059f5d550361cce
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-07 07:50:53 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,30 +14,30 @@ require 'pry'
     }
   }
   
-# def remove_strawberry(contacts)
-#   contacts.each do |person, details|
-#     if person=="Freddy mercury"
-#       details.each do |attribute, value|
-#         if attribute==:favorite_ice_cream_flavors
-#           value.delete_if do |name|
-#             name=="strawberry"
-#           end
-#         end
-#       end
-#     end
-# end
-#   contacts
-
-# end
-
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    if person == "Freddy Mercury"
-      contact_details_hash.each do |attribute, data|
-        if attribute == :favorite_ice_cream_flavors
-          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+  contacts.each do |person, details|
+    if person=="Freddy Mercury"
+      details.each do |attribute, value|
+        if attribute==:favorite_ice_cream_flavors
+          value.delete_if do |name|
+            name=="strawberry"
+          end
         end
       end
     end
-  end
 end
+  contacts
+
+end
+
+# def remove_strawberry(contacts)
+#   contacts.each do |person, contact_details_hash|
+#     if person == "Freddy Mercury"
+#       contact_details_hash.each do |attribute, data|
+#         if attribute == :favorite_ice_cream_flavors
+#           data.delete_if {|ice_cream| ice_cream == "strawberry"}
+#         end
+#       end
+#     end
+#   end
+# end
